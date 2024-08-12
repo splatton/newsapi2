52762581cb6cbadadbdbcf87a0ec97dd784bcb02
Tom Belanger <spltt.tlb@gmail.com>
2024-08-12 16:05:19 -0700

    try to update

---
+++ news_api_script.R
@@ -0,0 +1,12 @@
+library(httr2)
+req <- request("https://newsapi.org/v2/everything") |>
+  req_url_query(
+    q = '`"fools"`',
+    from = Sys.Date() - 1,
+    pageSize = 10,
+    apiKey = Sys.getenv("NEWS_API_KEY")
+  )
+
+resp <- req_perform(req)
+resp_body_json(resp)
+
